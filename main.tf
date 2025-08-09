26797ac9788e58017a2bf9c8d48cabf3b5d654a2
Zaid748-collab <zaidaj319@gmail.com>
2025-08-09 23:17:40 +0100

    update

--- main.tf
+++ main.tf
@@ -4,6 +4,9 @@ provider "aws" {
 resource "aws_s3_bucket" "terraformbuckettoday987654321" {
   bucket = "terraformbuckettoday987654321"
 }
+resource "aws_s3_bucket" "terraformbuckettoday9876543211" {
+  bucket = "terraformbuckettoday9876543211"
+}
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
